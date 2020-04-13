a4d392d6657f862fa7c36104fb37399fd51866e5
Saurabh Native <codeclassifiers@gmail.com>
2020-04-13 07:43:07 +0530

    header and login form added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+function Header() {
+    return(
+        <nav class="navbar navbar-dark bg-primary">
+            <div className="row col-12 d-flex justify-content-center text-white">
+            <span className="h3">Login</span>
+            </div>
+        </nav>
+    )
+}
+export default Header;
---
+++ src/components/LoginForm/LoginForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './LoginForm.css';
+function LoginForm() {
+    return(
+        <div className="card login-card">
+            card content
+        </div>
+    )
+}
+
+export default LoginForm;
