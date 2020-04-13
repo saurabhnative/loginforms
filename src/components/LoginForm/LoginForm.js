8cac46493d2c4ea6eca946379aa81d9b4d739ff7
Saurabh Native <codeclassifiers@gmail.com>
2020-04-13 07:51:33 +0530

    login form skeleton added

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -2,8 +2,21 @@ import React from 'react';
 import './LoginForm.css';
 function LoginForm() {
     return(
-        <div className="card login-card">
-            card content
+        <div className="card login-card mt-2 hv-center">
+            <form>
+                <div className="form-group text-left">
+                <label htmlFor="exampleInputEmail1">Email address</label>
+                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                </div>
+                <div className="form-group text-left">
+                <label htmlFor="exampleInputPassword1">Password</label>
+                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+                </div>
+                <div className="form-check">
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
         </div>
     )
 }
