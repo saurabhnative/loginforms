7adddd355f3bad3563ac8638730e540a6e6b78a5
Saurabh Native <codeclassifiers@gmail.com>
2020-04-13 08:55:43 +0530

    login forms added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 function Header() {
     return(
-        <nav class="navbar navbar-dark bg-primary">
+        <nav className="navbar navbar-dark bg-primary">
             <div className="row col-12 d-flex justify-content-center text-white">
             <span className="h3">Login</span>
             </div>
--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,21 +1,74 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
 import './LoginForm.css';
+import {API_BASE_URL} from '../../constants/apiContants';
 function LoginForm() {
+    const [state , setState] = useState({
+        email : "",
+        password : ""
+    })
+    const handleChange = (e) => {
+        const {id , value} = e.target   
+        setState(prevState => ({
+            ...prevState,
+            [id] : value
+        }))
+    }
+
+    const handleSubmitClick = (e) => {
+        e.preventDefault();
+        const payload={
+            "email":state.email,
+            "password":state.password,
+        }
+        axios.post(API_BASE_URL+'login', payload)
+            .then(function (response) {
+                if(response.data.code == 200){
+                    console.log("Login successfull");
+                }
+                else if(response.data.code == 204){
+                    console.log("Username password do not match");
+                }
+                else{
+                    console.log("Username does not exists");
+                }
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
     return(
         <div className="card login-card mt-2 hv-center">
             <form>
                 <div className="form-group text-left">
                 <label htmlFor="exampleInputEmail1">Email address</label>
-                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                <input type="email" 
+                       className="form-control" 
+                       id="email" 
+                       aria-describedby="emailHelp" 
+                       placeholder="Enter email" 
+                       value={state.email}
+                       onChange={handleChange}
+                />
                 <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
                 </div>
                 <div className="form-group text-left">
                 <label htmlFor="exampleInputPassword1">Password</label>
-                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+                <input type="password" 
+                       className="form-control" 
+                       id="password" 
+                       placeholder="Password"
+                       value={state.password}
+                       onChange={handleChange} 
+                />
                 </div>
                 <div className="form-check">
                 </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
+                <button 
+                    type="submit" 
+                    className="btn btn-primary"
+                    onClick={handleSubmitClick}
+                >Submit</button>
             </form>
         </div>
     )
