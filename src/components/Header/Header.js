19cb5e6b87757530d53db905ca79750bdb96a7f3
codeclassifiers <codeclassifiers@gmail.com>
2020-08-12 09:57:29 +0530

    Logout feature added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { withRouter } from "react-router-dom";
-
+import { ACCESS_TOKEN_NAME } from '../../constants/apiContants';
 function Header(props) {
     const capitalize = (s) => {
         if (typeof s !== 'string') return ''
@@ -10,10 +10,24 @@ function Header(props) {
     if(props.location.pathname === '/') {
         title = 'Welcome'
     }
+    function renderLogout() {
+        if(props.location.pathname === '/home'){
+            return(
+                <div className="ml-auto">
+                    <button className="btn btn-danger" onClick={() => handleLogout()}>Logout</button>
+                </div>
+            )
+        }
+    }
+    function handleLogout() {
+        localStorage.removeItem(ACCESS_TOKEN_NAME)
+        props.history.push('/login')
+    }
     return(
         <nav className="navbar navbar-dark bg-primary">
             <div className="row col-12 d-flex justify-content-center text-white">
                 <span className="h3">{props.title || title}</span>
+                {renderLogout()}
             </div>
         </nav>
     )
